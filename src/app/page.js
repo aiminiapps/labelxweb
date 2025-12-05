ddfbf38590abf183db584c745d3e4eef6fa12ed1
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-12-05 23:36:23 +0530

    Add AboutSection component to the Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import AboutSection from "@/components/about-section";
 import Hero from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Hero/>
+      <AboutSection/>
     </div>
   );
 }
