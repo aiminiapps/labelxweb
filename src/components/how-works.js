c3591968fe01f9512e602cf457d4b7e18af88567
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-12-06 23:41:08 +0530

    few unused import removed

--- src/components/how-works.js
+++ src/components/how-works.js
@@ -3,79 +3,20 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { 
   motion, 
-  useScroll, 
-  useTransform, 
-  useSpring,
   useMotionTemplate, 
   useMotionValue,
   animate,
-  useInView,
-  AnimatePresence
-} from 'framer-motion';
-// Using standard react-icons/fa for maximum compatibility
+  useInView
+} from 'motion/react';
 import { 
   FaWallet, 
-  FaBrain, 
   FaCoins, 
-  FaCheckDouble, 
-  FaUsers, 
-  FaArrowRight, 
-  FaRocket, 
-  FaShieldAlt,
   FaEye,
   FaComments,
   FaHeadphones,
-  FaLayerGroup,
-  FaBolt,
-  FaChartLine,
-  FaCode,
-  FaTerminal,
-  FaLink,
-  FaMousePointer,
   FaCheckCircle,
-  FaClock,
-  FaGlobeAmericas,
-  FaQuestion,
-  FaStar,
-  FaHistory,
-  FaToggleOn,
-  FaToggleOff,
-  FaEthereum,
-  FaCheck
+  FaQuestion
 } from 'react-icons/fa';
-
-// --- 1. Lenis Smooth Scroll Setup ---
-const useLenis = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://unpkg.com/@studio-freight/lenis@1.0.29/dist/lenis.min.js";
-    script.async = true;
-    script.onload = () => {
-      const lenis = new window.Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        direction: 'vertical',
-        gestureDirection: 'vertical',
-        smooth: true,
-        smoothTouch: false,
-        touchMultiplier: 2,
-      });
-
-      function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
-      requestAnimationFrame(raf);
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-};
-
-// --- 2. Micro-Interaction Icons ---
 
 const drawTransition = {
   duration: 1.2,
